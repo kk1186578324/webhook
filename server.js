4473d3ab8684466ff399f0fa3b4d363887d5899e
liukai <kai.liu@pku-lvxin.com>
2021-04-04 15:09:58 +0800

    v2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let server = http.createServer(function (req,res) {
-    if(req.method=='post'&&req.url==='/webhook'){
+    console.log(req.method,req.url)
+    if(req.method=='post'&&req.url=='/webhook'){
         res.setHeader('Content-Type','application/json');
         res.end(JSON.stringify({ok:true}))
     }else {
