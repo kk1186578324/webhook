9e6969a1d67437c93a2162dcd241835cd0e7a2bc
liukai <kai.liu@pku-lvxin.com>
2021-04-04 14:53:15 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+let http = require('http');
+let server = http.createServer(function (req,res) {
+    if(req.method=='post'&&req.url==='/webhook'){
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify({ok:true}))
+    }else {
+        res.end('Not Found')
+    }
+})
+server.listen(4000,()=>{
+    console.log('webhook服务已经在4000端口上启动')
+})
