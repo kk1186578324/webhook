417f769b15a7735619c5a402566d572db822f3f4
liukai <kai.liu@pku-lvxin.com>
2021-04-04 16:11:21 +0800

    v2

--- vue-back.sh
+++ vue-back.sh
@@ -2,10 +2,10 @@
 WORK_PATH='/usr/projects/vue-back'
 cd $WORK_PATH
 echo "先清除老代码"
-git reset --hard origin/master
+git reset --hard origin/main
 git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull origin main
 echo "开始执行构建"
 docker build -t vue-back:1.0 .
 echo "停止旧容器并删除旧容器"
